c6bdff1119b1bb7c58eaa8e010c7f9050d581a17
Peng Ye <yepeng230@gmail.com>
2023-10-02 20:30:59 +0800

    add function calcForceExertedBy method for class Planet

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -2,6 +2,8 @@ public class Planet {
     public double xxPos, yyPos, xxVel, yyVel, mass;
     public String imgFileName;
 
+    static final double Gconstant = 6.67e-11;
+
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
         yyPos = yP;
@@ -18,5 +20,20 @@ public class Planet {
         this.yyVel = p.yyVel;
         this.mass = p.mass;
         this.imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        double xdis_ = this.xxPos - p.xxPos;
+        double ydis_ = this.yyPos - p.yyPos;
+        return Math.sqrt(xdis_ * xdis_ + ydis_ * ydis_);
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = this.calcDistance(p);
+        return Gconstant * this.mass * p.mass / (r*r);
+    }
+
+    public double calForceExertedByX(Planet p) {
+      
     }
 }
