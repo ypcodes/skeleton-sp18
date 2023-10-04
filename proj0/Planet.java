2ec206456796e391d84e356da837219f51ffc072
Peng Ye <yepeng230@gmail.com>
2023-10-04 19:32:55 +0800

    add function to calculate net force in Planet

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,12 +5,13 @@ public class Planet {
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
-         yyPos = yP;
-         xxVel = xV;
-         yyVel = yV;
-         mass = m;
-         imgFileName = img;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
     }
+
     public Planet(Planet p) {
         this.xxPos = p.xxPos;
         this.yyPos = p.yyPos;
@@ -28,23 +29,19 @@ public class Planet {
 
     public double calcForceExertedBy(Planet p) {
         double r = this.calcDistance(p);
-        return GRAVITATIONAL_CONSTANT * this.mass * p.mass / (r*r);
+        return GRAVITATIONAL_CONSTANT * this.mass * p.mass / (r * r);
     }
 
     private double calcDistanceDiff(Planet p, char axis) {
+        // Calculate the difference between this planet and p along the given axis
         double diff = 0.0;
         if (axis == 'x') {
-            if(this.xxPos > p.xxPos) {
-                diff = this.xxPos - p.xxPos;
-            } else {
-                diff = -(this.xxPos - p.xxPos);
-            }
+            diff = this.xxPos - p.xxPos; // Subtract the x-positions
         } else if (axis == 'y') {
-            if(this.yyPos > p.yyPos) {
-                diff = this.yyPos - p.yyPos;
-            } else {
-                diff = -(this.yyPos - p.yyPos);
-            }
+            diff = this.yyPos - p.yyPos; // Subtract the y-positions
+        }
+        if (diff < 0) { // If the difference is negative
+            diff = -diff; // Make it positive by multiplying by -1
         }
         return diff;
     }
@@ -54,10 +51,36 @@ public class Planet {
         double dx = calcDistanceDiff(p, 'x');
         return force * dx / this.calcDistance(p);
     }
+
     public double calcForceExertedByY(Planet p) {
         double force = this.calcForceExertedBy(p);
         double dy = calcDistanceDiff(p, 'y');
         return force * dy / this.calcDistance(p);
     }
 
+    /** A Planets cannot exert gravitational forces on themselves */
+    private boolean equals(Planet p) {
+        if(this.xxPos == p.xxPos && this.yyPos == p.yyPos &&
+                this.xxVel == p.xxVel && this.yyVel == p.yyVel && this.mass == p.mass)
+            return true;
+        return false;
+    }
+
+    public double calcNetForceExertedByX(Planet[] p) {
+        double forces = 0;
+        for(Planet sp : p ) {
+            if(!this.equals(sp))
+                forces += this.calcForceExertedByX(sp);
+        }
+        return forces;
+    }
+
+    public double calcNetForceExertedByY(Planet[] p) {
+        double forces = 0;
+        for (Planet sp : p) {
+            if (!this.equals(sp))
+                forces += this.calcForceExertedByY(sp);
+        }
+        return forces;
+    }
 }
